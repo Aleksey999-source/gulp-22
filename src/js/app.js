4d5c91d757a4eb9b82161836bba7d8becde3020a
Aleksey <alexeyn525@gmail.com>
2022-07-31 21:32:11 +0300

    added mobile check code

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,19 @@ flsFunctions.isWebp()
 import Swiper, { Navigation, Pagination } from 'swiper'
 
 const swiper = new Swiper()
+
+// Проверка на тачскрин --------------------------------------------------
+let isMobile = 
+{
+   Android: function() {return navigator.userAgent.match(/Android/i);},
+   Blackberry: function() {return navigator.userAgent.match(/Blackberry/i);},
+   IOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+   Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+   Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+   any: function() {return isMobile.Android() || isMobile.Blackberry() || isMobile.IOS() || isMobile.Opera() || isMobile.Windows();}
+}
+
+let body = document.querySelector('body');
+
+isMobile.any() ? body.classList.add('touch') : body.classList.add('mouse');
+//
